f29ba88cff455d0d26721e9123596d7b6eac651f
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-04-21 12:56:44 +0200

    improvement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-
+import { createCountryListMarkup, createCountryMarkup, clearInput } from "./markups";
+export {countryList, countryInfo};
 
 const DEBOUNCE_DELAY = 300;
 
@@ -32,27 +33,4 @@ function onCountrySearch() {
     })
     .catch(error => console.log(error));
     clearInput()     
-}
-
-
-function createCountryListMarkup(arr) {
-    return arr
-    .map(({flags: {svg}, name: {official}}) =>
-    `<li><img class = "list-img" src="${svg}" alt="${official}"><p class = "country-list-name">${official}</p></li>`
-    ).join('')        
-}
-
-function createCountryMarkup(arr) {
-    return arr
-    .map(({name: {official}, capital, population, flags: {svg}, languages}) => 
-    `<div class="country-card"><img width="500" height="300" src="${svg}" alt="${official}"><h2>${official}</h2>
-    <p><b>Population:</b> ${population}</p>
-    <p><b>Language:</b> ${Object.values(languages)}</p>
-    <p><b>Capital:</b> ${capital}</p></div>
-    `).join('')
-}
-
-function clearInput() {
-    countryInfo.innerHTML = '';
-    countryList.innerHTML = '';
 }
